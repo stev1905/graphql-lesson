edc978811b074a5c3650ea6f94be23560280d447
Christian Stevens <cstevens@911MEMORIAL.ORG>
2020-01-31 13:36:13 -0500

    graphQL fully converted from Redux, redux folder deleted

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-boost';
 
-import { store, persistor } from './redux/store';
 
 import './index.css';
 
@@ -33,13 +30,9 @@ client.writeData({ data });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Provider store={store}>
       <BrowserRouter>
-        <PersistGate persistor={persistor}>
           <App />
-        </PersistGate>
       </BrowserRouter>
-    </Provider>
   </ApolloProvider>,
   document.getElementById('root')
 );
